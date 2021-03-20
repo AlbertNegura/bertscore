d1e14fae275a0270ae85b2b0e685e1e84f0b3087
I6145864 <a.negura@student.maastrichtuniversity.nl>
2021-03-20 14:54:51 +0100

    API Script

---
+++ inference.py
@@ -0,0 +1,38 @@
+import json
+import requests
+import datasets
+
+split_patterns = {
+        "train": "train[:80%]",
+        "validation": "train[80%:90%]",
+        "test": "train[90%:]"
+    }
+
+dataset_train = datasets.load_dataset('reddit_tifu', 'short', split=split_patterns["train"])
+dataset_val = datasets.load_dataset('reddit_tifu', 'short', split=split_patterns["validation"])
+dataset_test = datasets.load_dataset('reddit_tifu', 'short', split=split_patterns["test"])
+
+# body: ['documents']
+# title: ['title']
+
+
+headers = {"Authorization": f"Bearer {API_KEY}"}
+API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+
+def query(payload):
+    data = json.dumps(payload)
+    response = requests.request("POST", API_URL, headers=headers, data=data)
+    return json.loads(response.content.decode("utf-8"))
+
+data = []
+
+for i in range(len(dataset_train)):
+    data.append(query(
+        {
+            "inputs": dataset_train["documents"][i],
+        }
+    ))
+
+
+with open({OUTPUT_FILE}, 'w') as outfile:
+    json.dump(data, outfile)
--- transformers_model.py
+++ transformers_model.py
@@ -1,5 +1,18 @@
 import transformers
 import datasets
+import tensorflow_datasets as tfds
+from tensorflow_datasets.summarization import reddit_tifu, reddit_tifu_test
+from bert_score import score
+from bert_score import plot_example
+# hide the loading messages
+import logging
+import transformers
+transformers.tokenization_utils.logger.setLevel(logging.ERROR)
+transformers.configuration_utils.logger.setLevel(logging.ERROR)
+transformers.modeling_utils.logger.setLevel(logging.ERROR)
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+import tokenizer
 
 split_patterns = {
         "train": "train[:80%]",
@@ -16,3 +29,12 @@ dataset_test = datasets.load_dataset('reddit_tifu', 'short', split=split_pattern
 
 print(dataset_train.column_names)
 
+
+
+P, R, F1 = score(cands, refs, lang='en', verbose=True)
+print(tf.math.reduce_mean(P, axis=None, keepdims=False, name=None))
+print(tf.math.reduce_mean(R, axis=None, keepdims=False, name=None))
+print(tf.math.reduce_mean(F1, axis=None, keepdims=False, name=None))
+
+plot_example(cands[0],refs[0], lang="en")
+plot_example(cands[0],refs[0], lang="en", rescale_with_baseline=True)
