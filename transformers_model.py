dbb2d7ed99672ed8b61c6ef4eb3234fde1f0b599
I6145864 <a.negura@student.maastrichtuniversity.nl>
2021-03-20 13:20:44 +0100

    Transformers example modified to work with reddit-tifu

---
+++ transformers_model.py
@@ -0,0 +1,18 @@
+import transformers
+import datasets
+
+split_patterns = {
+        "train": "train[:80%]",
+        "validation": "train[80%:90%]",
+        "test": "train[90%:]"
+    }
+
+dataset_train = datasets.load_dataset('reddit_tifu', 'short', split=split_patterns["train"])
+dataset_val = datasets.load_dataset('reddit_tifu', 'short', split=split_patterns["validation"])
+dataset_test = datasets.load_dataset('reddit_tifu', 'short', split=split_patterns["test"])
+
+# body: ['documents']
+# title: ['title']
+
+print(len(dataset_train))
+
